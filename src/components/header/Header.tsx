9b2a3982b5899d2a1a6054d05dafabbb1a873a3a
Blackbrv <luthfiprtm@gmail.com>
2025-01-25 03:29:16 +0700

    Minor Change

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -5,11 +5,11 @@ const item = Array.from(Array(totalItem).keys());
 
 const Header = () => {
   return (
-    <div className="w-full h-max p-5 bg-[#0A1045] bg-red-500">
+    <div className="w-full h-max p-5 bg-[#0A1045]">
       <C.Carousel className="bg-green-500 w-full h-full p-5">
         <C.CarouselContent>
           {item.map((item, index) => (
-            <C.CarouselItem key={index}>{index}</C.CarouselItem>
+            <C.CarouselItem key={index}>{item}</C.CarouselItem>
           ))}
         </C.CarouselContent>
         <C.CarouselPrevious>ico</C.CarouselPrevious>
